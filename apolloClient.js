cc24e341466768fb12933ad74d203896bfdaf1d0
ytjdud <102743371+ytjdud@users.noreply.github.com>
2023-11-20 03:28:26 +0900

    Chore:

--- apolloClient.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-
-const client = new ApolloClient({
-  uri: 'api.openweathermap.org/data/2.5/forecast',
-  cache: new InMemoryCache(),
-});
-
-export default client;
