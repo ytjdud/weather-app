3173b9b4ddf50f714245ae72f21bb0814c9b3738
ytjdud <102743371+ytjdud@users.noreply.github.com>
2023-11-20 16:01:05 +0900

    Implement main page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 import earth from '/public/earth.png'
 
@@ -13,33 +14,35 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className={styles.welcome}>
-        <span style={{ color: 'grey' }}>Welcome to{'\n'}</span>
-        <span style={{ color: 'hotpink' }}>Weather App!</span>
-      </h1>
-      
-      <p className={styles.detail}>Choose a city from the list below to check the weather.</p>
-      
-      {/* <p>Hover</p> */}
+        <h1 className={styles.welcome}>
+          <span style={{ color: 'grey' }}>Welcome to{'\n'}</span>
+          <span style={{ color: 'hotpink' }}>Weather App!</span>
+        </h1>
+        
+        <p className={styles.detail}>Choose a city from the list below to check the weather.</p>
+        
+        {/* <p>Hover</p> */}
 
-      <div style={{position: 'relative'}}>
-        {/* <div className={styles.activeButton}>
-          
-          <button className={styles.button}>Seoul</button>
-          <p>Select</p>
-          <button className={styles.button}>Seoul</button>
-        </div> */}
+        <div style={{position: 'relative'}}>
+          {/* <div className={styles.activeButton}>
+            
+            <button className={styles.button}>Seoul</button>
+            <p>Select</p>
+            <button className={styles.button}>Seoul</button>
+          </div> */}
 
-        <div className={styles.cityButton}>
-          <button className={styles.button}>Seoul</button>
-          <button className={styles.button}>Tokyo</button>
-          <button className={styles.button}>Paris</button>
-          <button className={styles.button}>London</button>
+          <div className={styles.cityButton}>
+            <Link href="/Seoul"><button className={styles.button} >Seoul</button></Link>
+            <Link href="/Tokyo"><button className={styles.button} >Tokyo</button></Link>
+            <Link href="/Paris"><button className={styles.button} >Paris</button></Link>
+            <Link href="/London"><button className={styles.button} >London</button></Link>
+          </div>
         </div>
-      </div>
 
-      <Image src={earth} alt='지구' className={styles.earthImg}/>
-      
-    </div>
+        <Image 
+        src={earth} alt='지구' 
+        width="350vmin" height="350vmin" />
+        
+      </div>
   )
 }
